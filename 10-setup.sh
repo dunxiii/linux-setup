1c511006f2c80c6089aa6ba84031f242346a0ee4
Jack Söderblom <dunxiii@gmail.com>
2018-08-19 23:32:55 +0200

    Added initial files

---
+++ 10-setup.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+set -au
+
+install_packages+=(
+    #default-jre
+    #libnss3-tools
+    #ntp
+    #python-keyring # replaced by pip keyring
+    #ranger
+    arc-theme
+    chrome-gnome-shell
+    curl
+    git
+    gnome-tweaks
+    google-chrome-stable
+    gpick
+    htop
+    network-manager-openconnect-gnome # Only in Ubuntu
+    nextcloud-client
+    nextcloud-client-nautilus
+    openssh-server
+    papirus-icon-theme
+    telegram-desktop
+    terminator
+    unrar # requires non-free debian repo
+    vim
+)
+
+if [ "$(lsb_release -is)" = "Ubuntu" ]; then
+
+    install_packages+=(
+        ranger
+    )
+
+elif [ "$(lsb_release -is)" = "Debian" ]; then
+
+    install_packages+=(
+        plymouth
+        plymouth-themes
+    )
+
+    purge_packages+=(
+        #cheese
+        #gnome-calendar
+        #gnome-terminal
+        #gnome-todo
+        #rhythmbox
+        #shotwell
+        gnome-games
+    )
+
+    # Enable contrib and non-free repositorys
+    sed -i '/^# /! s/main$/main contrib non-free/' /etc/apt/sources.list
+
+fi
+
+#install_pip+=(
+#    keyring # May need python-secretstorage from apt
+#)
+
+# Make sure script is run as root
+if [ ${UID} -ne 0 ]; then
+    echo "Run this script as root" 1>&2
+    exit 2
+fi
+
+# Repo for chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+
+# Repo for nextcloud
+add-apt-repository -y ppa:nextcloud-devs/client
+
+# Bring system up to date
+apt-get update
+apt-get -V dist-upgrade
+
+#apt-get install dirmngr # Reqirement for papirus repo key
+apt-get -V install "${install_packages[@]}"
+apt-get -V purge "${purge_packages[@]}"
+apt-get -V autoremove
+
+test -n "${install_pip:-}" && \
+    pip install --ignore-installed ${install_pip[@]}
+
+# Passwordless sudo
+if ! grep -q NOPASSWD /etc/sudoers; then
+    echo "%sudo   ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
+fi
+
+if [ "$(lsb_release -is)" = "Debian" ]; then
+
+    # Plymouth
+    sed -i -e '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"quiet"/"quiet splash"/' /etc/default/grub
+    plymouth-set-default-theme -R softwaves
+    update-grub2
+
+    # Fix for chrome gnome extentions support
+    sudo mkdir -p /etc/opt/chrome/native-messaging-hosts
+    sudo cp /etc/chromium/native-messaging-hosts/org.gnome.chrome_gnome_shell.json /etc/opt/chrome/native-messaging-hosts/
+
+fi
+
+echo "Install these gnome extentions:"
+echo "- Refresh wifi connections"
+echo "- User Themes"
---
+++ 20-setup.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -au
+
+# Fix for user shell themes, so it can be set with gsettings
+# Reference: https://bbs.archlinux.org/viewtopic.php?id=139586
+sudo cp ~/.local/share/gnome-shell/extensions/user-theme@gnome-shell-extensions.gcampax.github.com/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas 
+
+# Shell theme
+gsettings set org.gnome.shell.extensions.user-theme name "Arc-Dark"
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Darker"
+
+# Icon theme
+gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+
+# Misc gnome settings
+gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface enable-animations false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
+gsettings set org.gnome.settings-daemon.plugins.orientation active false
+gsettings set org.gnome.shell favorite-apps "['terminator.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.gedit.desktop', 'google-chrome.desktop', 'firefox-esr.desktop', 'org.gnome.Screenshot.desktop']"
+
+# Nautilus
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.gnome.nautilus.preferences show-delete-permanently true
+gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+
+# Gedit
+gsettings set org.gnome.gedit.preferences.editor auto-indent true
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor display-overview-map true
+gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+gsettings set org.gnome.gedit.preferences.editor scheme 'solarized-light'
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.ui side-panel-visible true
+gsettings set org.gnome.gedit.state.window side-panel-active-page 'GeditFileBrowserPanel'
+
+# Power settings
+gsettings set org.gnome.desktop.session idle-delay 'uint32 600'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
+                                                                                                          
+# Home dir setup
+[[ ! -d ~/git ]] && mkdir ~/git
+
+# Remove exsisting dirs
+[[ -d ~/Desktop ]] && rmdir ~/Desktop
+[[ -d ~/Music ]] && rmdir ~/Music
+[[ -d ~/Videos ]] && rmdir ~/Videos
+#[[ ! -L ~/.config/argos ]] && rm -rf ~/.config/argos
+#[[ ! -L ~/.config/terminator ]] && rm -rf ~/.config/terminator
+#[[ ! -L ~/.ssh ]] && rm -rf ~/.ssh
+
+# fzf
+#[[ ! -d ~/git/fzf ]] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/git/fzf && \
+#    ~/git/fzf/install
+#[[ ! -d ~/git/fzf-marks ]] && git clone https://github.com/urbainvaes/fzf-marks.git ~/git/fzf-marks
