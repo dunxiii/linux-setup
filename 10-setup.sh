1204722b11f51b2a523c94f57120d5f96ba94c09
Jack Söderblom <jack.soderblom@bahnhof.net>
2018-11-21 22:24:35 +0100

    Added repo for newer bluez version

--- 10-setup.sh
+++ 10-setup.sh
@@ -85,6 +85,9 @@ if [ ${UID} -ne 0 ]; then
     exit 2
 fi
 
+# Repo for bluez
+add-apt-repository ppa:bluetooth/bluez
+
 # Repo for chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
