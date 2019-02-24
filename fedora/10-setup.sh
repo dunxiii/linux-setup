ec733bfc599e92a84b27513c95ccd674f577d334
Jack Söderblom <dunxiii@gmail.com>
2019-02-24 18:12:43 +0100

    Added fedora files

---
+++ fedora/10-setup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+set -au
+
+install_packages+=(
+    NetworkManager-openconnect-gnome
+    code
+    curl
+    dconf-editor
+    git
+    gnome-tweaks
+    google-chrome-stable
+    gpick
+    htop
+    nmap
+    pwgen
+    python3-virtualenv
+    remmina
+    snapd
+    transmission
+    unar
+    vim
+)
+
+# nVidia driver from RPM Fusion
+install_packages+=(
+    akmod-nvidia
+    xorg-x11-drv-nvidia
+)
+
+# vscode snap does not work any good
+snap_packages+=(
+    nextcloud-client # Works better than repo version
+    spotify
+    vlc
+)
+
+# Make sure script is run as root
+if [ ${UID} -ne 0 ]; then
+    echo "Run this script as root" 1>&2
+    exit 2
+fi
+
+# Enable google chrome repo
+dnf install fedora-workstation-repositories
+dnf config-manager --set-enabled google-chrome
+dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
+
+# Repo for VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+
+# Updates each package to the latest version that is both available and resolvable
+dnf upgrade
+
+# Install extra packages
+dnf install "${install_packages[@]}"
+
+# Remove old dependencies
+dnf autoremove
+
+# Install snap packages
+snap install "${snap_packages[@]}"
+
+echo "Good gnome extentions to install:"
+echo "- Argos"
+echo "- Clipboard Indicator"
+echo "- Dash to Panel"
+echo "- No Topleft Hot Corner"
+echo "- Refresh wifi connections"
+echo "- Top Panel Workspace Scroll"
+echo "- Topicons plus"
---
+++ fedora/20-setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -au
+
+# Misc gnome settings
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
+
+# GNOME Terminal
+gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
+# Nautilus
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+#gsettings set org.gnome.nautilus.preferences show-delete-permanently true
+gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+
+# Power settings
+#gsettings set org.gnome.desktop.session idle-delay 'uint32 600'
+#gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+#gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+#gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'suspend'
+
+# Home dir setup
+[[ ! -d ~/git ]] && mkdir ~/git
+
+# Install VSCode extentions
+vscode_ext+=(
+    #DavidAnson.vscode-markdownlint
+    #eamodio.gitlens
+    #eamodio.toggle-excluded-files
+    #PKief.markdown-checkbox
+    #robertohuertasm.vscode-icons
+    #shd101wyy.markdown-preview-enhanced
+    #vscoss.vscode-ansible
+    #zhuangtongfa.Material-theme
+)
+
+#for extension in "${vscode_ext[@]}"; do
+#    code --install-extension "${extension}"
+#done
