425181addb2b5c7dd25b49ada8ca2b91711bc6b2
Jack Söderblom <jack.soderblom@bahnhof.net>
2019-04-09 19:55:39 +0200

    Installing VSCode as a snap

--- fedora/10-setup.sh
+++ fedora/10-setup.sh
@@ -40,7 +40,7 @@ fi
 
 # vscode snap does not work any good
 snap_packages+=(
-    nextcloud-client # Works better than repo version
+    'code --classic'
     spotify
     vlc
 )
@@ -58,10 +58,6 @@ dnf config-manager --set-enabled google-chrome
 grep -i nvidia <(lspci) && \
     dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
 
-# Repo for VSCode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
-
 # Repo for docker
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
@@ -75,7 +71,9 @@ dnf install "${install_packages[@]}"
 dnf autoremove
 
 # Install snap packages
-snap install "${snap_packages[@]}"
+for snap in "${snap_packages[@]}"; do
+    snap install "${snap}"
+done
 
 echo "Good gnome extentions to install:"
 echo "- Argos"
