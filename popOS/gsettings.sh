5c6f978d6644ab3bc40f358767ffab42e625ec95
Jack Söderblom <dunxiii@gmail.com>
2019-10-22 21:42:44 +0200

    Added dash-to-dock settings

--- popOS/gsettings.sh
+++ popOS/gsettings.sh
@@ -17,3 +17,12 @@ gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 
 ## Power settings
 gsettings set org.gnome.desktop.session idle-delay 'uint32 900' # 15min untill screen blanks
+
+## Dash to dock
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-color '#f57900'
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES'
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#f57900'
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 1
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.5
