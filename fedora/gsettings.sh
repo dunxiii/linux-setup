bf155bc0a075146b37384d25338fbb1d9bee7b2a
Jack Söderblom <dunxiii@gmail.com>
2019-10-29 21:23:46 +0100

    Update fedora

--- fedora/gsettings.sh
+++ fedora/gsettings.sh
@@ -8,12 +8,14 @@ set -au
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
 gsettings set org.gnome.system.locale region 'sv_SE.UTF-8'
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'chromium-browser.desktop', 'firefox.desktop', 'spotify_spotify.desktop', 'code.desktop', 'org.gnome.Calculator.desktop', 'org.remmina.Remmina.desktop', 'nextcloud.desktop', 'virt-manager.desktop']"
+
+# Touchpad / Touchscreen
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.settings-daemon.peripherals.touchscreen orientation-lock true
 
 # Notifications
 gsettings set org.gnome.desktop.notifications show-in-lock-screen false
